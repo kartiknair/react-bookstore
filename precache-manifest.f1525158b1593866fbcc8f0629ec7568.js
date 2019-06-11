910e2bd09213c6bc9e0a9ac6216430098df1d7e8
Kartik Nair <kartiknair1911@gmail.com>
2019-06-11 13:08:36 +0400

    Updates

---
+++ precache-manifest.f1525158b1593866fbcc8f0629ec7568.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a4c54a1dfd309894a4ac155f5ff9070",
+    "url": "/react-bookstore/index.html"
+  },
+  {
+    "revision": "5d23144f2ca7792d7774",
+    "url": "/react-bookstore/static/css/main.5cf0c4e9.chunk.css"
+  },
+  {
+    "revision": "490d363a9f3b96d83a31",
+    "url": "/react-bookstore/static/js/2.105e8d13.chunk.js"
+  },
+  {
+    "revision": "5d23144f2ca7792d7774",
+    "url": "/react-bookstore/static/js/main.d924cfa1.chunk.js"
+  },
+  {
+    "revision": "b8e11062849f44b0a064",
+    "url": "/react-bookstore/static/js/runtime~main.aa841d1b.js"
+  },
+  {
+    "revision": "a80ffdd2c5772b5c71c08d578a17e662",
+    "url": "/react-bookstore/static/media/readmoreFullLogo.a80ffdd2.svg"
+  }
+]);
